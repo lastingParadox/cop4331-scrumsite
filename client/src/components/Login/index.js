8ce11014b18fef44df1bddbe91ddd425126a85f0
DonovanReynolds <donovanreynolds123@gmail.com>
2023-04-18 17:54:23 -0400

    Login/Register Page
    More or less finished the login/register page. Maybe has some kinks to be tested later.

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,80 +1,258 @@
-import React, {useState, useEffect, Component} from "react";
-import {Helmet} from "react-helmet";
+import React, { useState, useEffect, Component } from "react";
 import "./Login.css";
+import Switch from "react-switch";
 
-//Complete 100% prototype of login
-//Mostly just me playing with react and css
+import useWindowSize from "../windowSize";
 
+export default function Login() {
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
-export default function Login(){
+  const [color, setColor] = useState("white");
+  const [checked, setChecked] = useState(false);
+  const [signIn, setSignIn] = useState(false);
+  const size = useWindowSize();
 
-   const lightModeLoginBox = {
-      border: '2px black',
-      width: '50%'
-   };
+  function toggleColor() {
+    if (color === "white") {
+      setColor("black");
+    } else {
+      setColor("white");
+    }
+  }
 
-   const usernameBox = <div>
-                        <center>
-                           <label
-                           for="username"><b>Username</b></label>
-                           <input  style={lightModeLoginBox} className="login-boxes" type = "text" placeholder = "Enter Username" name = "username"  maxlength = "110" required
-                           onChange={(e) => setUsername(e.target.value)}
-                           ></input>
-                        </center>
-                       </div>
+  const switchColor = (nextChecked) => {
+    setChecked(nextChecked);
+    toggleColor();
+  };
 
-   const [password,setPassword] = useState("");
-   const [username,setUsername] = useState("");
-   const [color,setColor] = useState("black");
+  useEffect(() => {
+    document.body.style.backgroundColor = color;
+  }, [color]);
 
-   const toggleColor = color => {
-      if (color === "white")
-         setColor("black");
-      else
-      {
-         setColor("white");
-      }
-   }
+  const login = (
+    <div>
+      <form className="login">
+        <h1 className={color === "white" ? "login-light" : "login-dark"}>
+          Login
+        </h1>
+        <label
+          className={color === "white" ? "text-light" : "text-dark"}
+          for="username"
+        >
+          <b>Username</b>
+        </label>
+        <input
+          className={
+            color === "white" ? "login-boxes-light" : "login-boxes-dark"
+          }
+          type="text"
+          placeholder="Enter Username"
+          name="username"
+          maxlength="110"
+          required
+          onChange={(e) => setUsername(e.target.value)}
+        ></input>
+        <label
+          className={color === "white" ? "text-light" : "text-dark"}
+          for="password"
+        >
+          <b>Password</b>
+        </label>
+        <input
+          className={
+            color === "white" ? "login-boxes-light" : "login-boxes-dark"
+          }
+          type="password"
+          placeholder="Enter Password"
+          name="password"
+          maxlength="110"
+          required
+          onChange={(e) => setPassword(e.target.value)}
+        ></input>
+        <button className="login-button" type="submit">
+          Sign in
+        </button>
+      </form>
+    </div>
+  );
 
-   const button = <button onClick = {
-      () => toggleColor(color)
-   } >testColorSwitch</button>
-   useEffect(() => {
-      document.body.style.backgroundColor = color
-   },[color])
+  const register = (
+    <div>
+      <form className="login">
+        <h1 className={color === "white" ? "login-light" : "login-dark"}>
+          register
+        </h1>
+        <label
+          className={color === "white" ? "text-light" : "text-dark"}
+          for="username"
+        >
+          <b>Email</b>
+        </label>
+        <input
+          className={
+            color === "white" ? "login-boxes-light" : "login-boxes-dark"
+          }
+          type="text"
+          placeholder="Enter Email"
+          maxlength="110"
+          required
+          onChange={(e) => setUsername(e.target.value)}
+        ></input>
+        <label
+          className={color === "white" ? "text-light" : "text-dark"}
+          for="username"
+        >
+          <b>First Name</b>
+        </label>
+        <input
+          className={
+            color === "white" ? "login-boxes-light" : "login-boxes-dark"
+          }
+          type="text"
+          placeholder="Enter First Name"
+          maxlength="110"
+          required
+          onChange={(e) => setFirstName(e.target.value)}
+        ></input>
+        <label
+          className={color === "white" ? "text-light" : "text-dark"}
+          for="username"
+        >
+          <b>Last Name</b>
+        </label>
+        <input
+          className={
+            color === "white" ? "login-boxes-light" : "login-boxes-dark"
+          }
+          type="text"
+          placeholder="Enter Username"
+          maxlength="110"
+          required
+          onChange={(e) => setLastName(e.target.value)}
+        ></input>
+        <label
+          className={color === "white" ? "text-light" : "text-dark"}
+          for="password"
+        >
+          <b>Password</b>
+        </label>
+        <input
+          className={
+            color === "white" ? "login-boxes-light" : "login-boxes-dark"
+          }
+          type="password"
+          placeholder="Enter Password"
+          maxlength="110"
+          required
+          onChange={(e) => setPassword(e.target.value)}
+        ></input>
+        <label
+          className={color === "white" ? "text-light" : "text-dark"}
+          for="password"
+        >
+          <b>Confirm Password</b>
+        </label>
+        <input
+          className={
+            color === "white" ? "login-boxes-light" : "login-boxes-dark"
+          }
+          type="password"
+          placeholder="Enter Password Again"
+          maxlength="110"
+          required
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        ></input>
+        <button className="login-button" type="submit">
+          Register
+        </button>
+      </form>
+    </div>
+  );
 
+  const switchBox = (
+    <>
+      {" "}
+      <label>
+        <span className={color === "white" ? "text-light" : "text-dark"}>
+          {" "}
+          Dark Mode
+        </span>
+        <Switch
+          checked={checked}
+          onChange={switchColor}
+          onColor="#121212"
+          onHandleColor="#a6a6a6"
+          handleDiameter={30}
+          uncheckedIcon={false}
+          checkedIcon={false}
+          boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+          activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
+          height={20}
+          width={48}
+          className="react-switch"
+          id="material-switch"
+        />
+      </label>
+    </>
+  );
 
-
-   return(
-      <>
-         <h1 className = "login"><center>Login</center>
-         </h1>
-         <form>
-            <div>
-               {usernameBox}
+  return (
+    <>
+      {size.width > 1040 ? (
+        <>
+          <div className="login-container">
+            <div
+              className={`fill-box-${color === "white" ? "light" : "dark"} ${
+                signIn ? "" : "right"
+              }`}
+            >
+              <label className={color === "white" ? "text-light" : "text-dark"}>
+                {signIn
+                  ? "Click here if you already have an account"
+                  : "Click here to make a new account"}
+              </label>
+              <button
+                className="switch-button"
+                onClick={() => setSignIn(!signIn)}
+              >
+                {signIn ? "Register" : "Login"}
+              </button>
             </div>
-            
-            <div>
-               <center>
-                  <label for="password"><b>Password</b></label>
-                     <input className="login-boxes" type = "password" placeholder = "Enter Password" name = "password"  maxlength = "110" required
-                     onChange={(e) => setPassword(e.target.value)}
-                     ></input>
-               </center>
-            </div>
-            <div>
-               <center>
-                  <button type = "submit">Login</button>
-               </center>
-            </div> 
+            {login}
+            {register}
+          </div>
+        </>
+      ) : (
+        <div className="login-container">
+          {!signIn ? <>{login}</> : <>{register}</>}
+          <div className="center">
+            <label className={color === "white" ? "text-light" : "text-dark"}>
+              {signIn ? "Change to Sign in" : "Change to Register"}
+            </label>
+            <Switch
+              checked={signIn}
+              onChange={() => {
+                setSignIn(!signIn);
+              }}
+              onColor="#248CFD"
+              onHandleColor="#4fa3fc"
+              handleDiameter={30}
+              uncheckedIcon={false}
+              checkedIcon={false}
+              height={20}
+              width={48}
+              className="react-switch"
+              id="material-switch"
+            />
+          </div>
+        </div>
+      )}
 
-         </form>
-
-         <div className="white-text"> This is the username being typed {username}</div>
-         <div className="white-text"> This is the password being typed {password}</div>
-         {button}
-      </>
-   )
-
-
+      <div className="footer">{switchBox}</div>
+    </>
+  );
 }

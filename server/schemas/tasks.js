427de7367cfd749146bd4b3e45d1ec787ddee563
lastingParadox <zanpres@gmail.com>
2023-04-18 20:03:49 -0400

    Add list schema, add hooks upon deletion and creation of tasks

--- server/schemas/tasks.js
+++ server/schemas/tasks.js
@@ -1,9 +1,10 @@
 import mongoose from 'mongoose';
 const { Schema } = mongoose;
+import List from './lists.js'
 
 let taskSchema = new Schema( {
     title: { type: String, required: true },
-    list: { type: String, required: true },
+    list: { type: Schema.Types.ObjectId, ref: 'List', required: true },
     workspace: { type: Schema.Types.ObjectId, ref: 'Workspace', required: true },
     author: { type: SchemaType.Types.ObjectId, ref: 'User', required: true },
     description: String,
@@ -11,6 +12,33 @@ let taskSchema = new Schema( {
     assignees: [
         { type: Schema.Types.ObjectId, ref: 'User' },
     ],
+});
+
+taskSchema.pre('findOneAndDelete', async function(next) {
+    const doc = await this.model.findOne(this.getQuery());
+    await List.updateMany({ _id: doc.list }, {
+        $pull: {
+            tasks: {
+                $in: doc._id,
+            },
+        }
+    });
+    next();
+});
+
+taskSchema.pre('save', async function (next) {
+    if (!isNew) next();
+    try {
+        const list = await List.findById(this.list);
+        if (!list) {
+            throw new Error('List not found');
+        }
+        list.tasks.push(this._id);
+        await list.save();
+        next();
+    } catch (error) {
+        next(error);
+    }
 });
 
 const Task = mongoose.model('Task', taskSchema);

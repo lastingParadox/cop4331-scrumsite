e98ed684e7249eea9c770f8636399e69233872c2
lastingParadox <zanpres@gmail.com>
2023-04-18 23:27:34 -0400

    Fix user save pre hook

--- server/schemas/users.js
+++ server/schemas/users.js
@@ -1,6 +1,7 @@
 import bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
 import Workspace from './workspaces.js';
+import List from './lists.js';
 const { Schema } = mongoose;
 
 let validateEmail = function(email) {
@@ -29,9 +30,18 @@ userSchema.pre('save', async function (next) {
     // Workspace addition on new User
     const user = this;
     if (user.isNew && user.workspaces.length === 0) {
+
+        const list1 = { title: "To-Do", };
+        const list2 = { title: "In Progress" };
+        const list3 = { title: "Completed" };
+
+        await list1.save();
+        await list2.save();
+        await list3.save();
+
         const privateWorkspace = new Workspace({
             title: `${user.firstName}'s Workspace`,
-            lists: ["To-Do", "In Progress", "Completed"],
+            lists: [ list1._id, list2._id, list3._id ],
             members: [ user._id ],
         });
 

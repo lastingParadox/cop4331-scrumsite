63f718fbc0873e30b3bfb8a554a39f3689c6d29b
lastingParadox <zanpres@gmail.com>
2023-04-18 20:00:22 -0400

    Update workspace routes, create task routes

---
+++ server/routes/tasks.js
@@ -0,0 +1,146 @@
+import express from 'express';
+const router = express.Router();
+import Task from '../models/task.js';
+
+// Get all tasks
+router.get('/tasks', async (req, res) => {
+/*
+ *  GET: Retrieves all tasks
+ *  Note: Not particularly useful from a user perspective. See /auth/tasks for a route
+ *        that will show all the tasks a user has access to.
+ */
+
+    try {
+        const tasks = await Task.find();
+        res.json(tasks);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+// Get one task
+router.get('/tasks/:id', getTask, (req, res) => {
+/*
+ *  GET: Retrieves a task with the ID provided.
+ *  Required: ID Argument, where ID is the task's object id. (task.id)
+ *  Note: ID IS NOT AN INTEGER. IT'S A STRING.
+ */
+
+    res.json(res.task);
+});
+
+// Create one task
+router.post('/tasks', async (req, res) => {
+/*
+ *  POST: Creates a task
+ *  Body: {
+ *          "title": "Example Title",
+ *          "list": "Example List 1",
+ *          "workspace": "643c55f531534439b6b8700b",
+ *          "author": cookie.id,
+ *          "description": "Example description",
+ *          "dueDate": new Date("12-10-23"),
+ *          "assignees": [ cookie.id ]
+ *  }
+ *  Required: Title, List, Workspace, Author
+ */
+    const task = new Task({
+        title: req.body.title,
+        list: req.body.list,
+        workspace: req.body.workspace,
+        author: req.body.author,
+        description: req.body.description,
+        dueDate: req.body.dueDate,
+        assignees: req.body.assignees,
+    });
+
+    try {
+        const newTask = await task.save();
+        res.status(201).json(newTask);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+    });
+
+// Update one task
+router.patch('/tasks/:id', getTask, async (req, res) => {
+/*
+ *  POST: Creates a task
+ *  Body: {
+ *          "title": "Example Title",
+ *          "list": "Example List 1",
+ *          "workspace": "643c55f531534439b6b8700b",
+ *          "author": cookie.id,
+ *          "description": "Example description",
+ *          "dueDate": new Date("12-10-23"),
+ *          "assignees": [ cookie.id ]
+ *  }
+ *  Required: ID Argument, where ID is the task's object id. (task.id)
+ *  Note: ID IS NOT AN INTEGER. IT'S A STRING.
+ */
+    if (req.body.title != null) {
+        res.task.title = req.body.title;
+    }
+
+    if (req.body.list != null) {
+        res.task.list = req.body.list;
+    }
+
+    if (req.body.workspace != null) {
+        res.task.workspace = req.body.workspace;
+    }
+
+    if (req.body.author != null) {
+        res.task.author = req.body.author;
+    }
+
+    if (req.body.description != null) {
+        res.task.description = req.body.description;
+    }
+
+    if (req.body.dueDate != null) {
+        res.task.dueDate = req.body.dueDate;
+    }
+
+    if (req.body.assignees != null) {
+        res.task.assignees = req.body.assignees;
+    }
+
+    try {
+        const updatedTask = await res.task.save();
+        res.json(updatedTask);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+// Delete one task
+router.delete('/tasks/:id', getTask, async (req, res) => {
+/*
+ *  DELETE: Deletes a task with the ID provided.
+ *  Required: ID Argument, where ID is the task's object id. (task.id)
+ *  Note: ID IS NOT AN INTEGER. IT'S A STRING.
+ */
+    try {
+        await res.task.remove();
+        res.json({ message: 'Task deleted' });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+// Middleware function to get a single task by ID
+async function getTask(req, res, next) {
+    try {
+        const task = await Task.findById(req.params.id);
+        if (task == null) {
+            return res.status(404).json({ message: 'Cannot find task' });
+        }
+        res.task = task;
+        next();
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
+}
+
+export default router;
--- server/routes/workspaces.js
+++ server/routes/workspaces.js
@@ -2,131 +2,121 @@ import express from 'express'
 const router = express.Router();
 import Workspace from '../schemas/workspaces';
 
-// Create a workspace
-router.post('/workspaces', async (req, res) => {
-/*
- *  POST: Creates a Workspace
- *  Body: { "title": "Example Title", "lists": ["Example List 1", "Example List 2"], "members": [ userObjectId1, userObjectId2 ] }
- *  Required: Title, Members (at least one user object id in array)
- */
-
-    try {
-        let { title, lists, members } = req.body;
-
-        if (!lists || lists.length === 0) {
-            lists = ['To-Do", "In Progess, "Completed']
-        }
-
-        // Create a new workspace object
-        const workspace = new Workspace({ title, lists, members });
-
-        // Save the workspace to the database
-        await workspace.save();
-
-        // Send a response with the new workspace object
-        res.status(201).json({ workspace });
-
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
-
 // Get all workspaces
-router.get('/workspaces', async (req, res) => {
+router.get('/', async (req, res) => {
 /*
  *  GET: Retrieves ALL Workspaces.
  *  Note: Not particularly useful from a user perspective. See /auth/workspaces for a route
  *        that will show all the workspaces a user has access to.
  */
-
     try {
         const workspaces = await Workspace.find();
-
-        res.status(200).json({ workspaces });
-
-    } catch (error) {
-        res.status(500).json({ error: error.message });
+        res.status(200).json(workspaces);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
     }
 });
 
-// Get a single workspace by ID
-router.get('/workspaces/:id', async (req, res) => {
+// Create a new workspace
+router.post('/', async (req, res) => {
+/*
+ *  POST: Creates a Workspace
+ *  Body: {
+ *          "title": "Example Title",
+ *          "lists": ["Example List 1", "Example List 2"],
+ *          "members": [ userObjectId1, userObjectId2 ]
+ *  }
+ *  Required: Title, Members (at least one user object id in array)
+ */
+    const workspace = new Workspace({
+        title: req.body.title,
+        lists: req.body.lists,
+        members: req.body.members
+    });
+
+    try {
+        const newWorkspace = await workspace.save();
+        res.status(201).json(newWorkspace);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+// Get a single workspace
+router.get('/:id', getWorkspace, (req, res) => {
 /*
  *  GET: Retrieves a workspace with the ID provided.
  *  Required: ID Argument, where ID is the workspace's object id. (workspace.id)
  *  Note: ID IS NOT AN INTEGER. IT'S A STRING.
  */
-
-    try {
-        const workspace = await Workspace.findById(req.params.id);
-
-        if (!workspace) {
-            return res.status(404).json({ message: 'Workspace not found' });
-        }
-
-        res.status(200).json({ workspace });
-
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+    res.json(res.workspace);
 });
 
-// Update a workspace by ID
-router.put('/workspaces/:id', async (req, res) => {
+// Update a workspace
+router.patch('/:id', getWorkspace, async (req, res) => {
 /*
- *  PUT: Updates a workspace with the ID provided.
- *  Body: { "title": "Example Title", "lists": ["Example List 1", "Example List 2"], "members": [ userObjectId1, userObjectId2 ] }
+ *  PATCH: Updates a workspace with the ID provided.
+ *  Body: {
+ *          "title": "Example Title",
+ *          "lists": ["Example List 1", "Example List 2"],
+ *          "members": [ userObjectId1, userObjectId2 ]
+ *  }
  *  Required: ID Argument, where ID is the workspace's object id. (workspace.id)
  *  Note: ID IS NOT AN INTEGER. IT'S A STRING.
  */
+    if (req.body.title != null) {
+        res.workspace.title = req.body.title;
+    }
+
+    if (req.body.lists != null) {
+        res.workspace.lists = req.body.lists;
+    }
+
+    if (req.body.members != null) {
+        res.workspace.members = req.body.members;
+    }
 
     try {
-        const workspace = await Workspace.findById(req.params.id);
-
-        if (!workspace) {
-            return res.status(404).json({ message: 'Workspace not found' });
-        }
-
-        const { title, lists, members } = req.body;
-
-        // Update the workspace object
-        workspace.title = title || workspace.title;
-        workspace.lists = lists || workspace.lists;
-        workspace.members = members || workspace.members;
-
-        // Save the updated workspace to the database
-        await workspace.save();
-
-        res.status(200).json({ workspace });
-
-    } catch (error) {
-        res.status(500).json({ error: error.message });
+        const updatedWorkspace = await res.workspace.save();
+        res.json(updatedWorkspace);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
     }
 });
 
-// Delete a workspace by ID
-router.delete('/workspaces/:id', async (req, res) => {
+// Delete a workspace
+router.delete('/:id', getWorkspace, async (req, res) => {
 /*
  *  DELETE: Deletes a workspace with the ID provided.
  *  Required: ID Argument, where ID is the workspace's object id. (workspace.id)
  *  Note: ID IS NOT AN INTEGER. IT'S A STRING.
  */
-
     try {
-        const workspace = await Workspace.findById(req.params.id);
-
-        if (!workspace) {
-            return res.status(404).json({ message: 'Workspace not found' });
-        }
-
-        // Delete the workspace from the database
-        await workspace.remove();
-
-        res.status(200).json({ message: 'Workspace deleted' });
-
-    } catch (error) {
-        res.status(500).json({ error: error.message });
+        await res.workspace.remove();
+        res.json({ message: 'Workspace deleted' });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
     }
 });
+
+
+
+// Middleware function to get a single workspace by ID
+async function getWorkspace(req, res, next) {
+    let workspace;
+
+    try {
+        workspace = await Workspace.findById(req.params.id);
+
+        if (workspace == null) {
+        return res.status(404).json({ message: 'Cannot find workspace' });
+        }
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
+
+    res.workspace = workspace;
+    next();
+}
 
 export default router;

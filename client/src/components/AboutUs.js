deb4877cc3d7774150b9f6a9bb903ee103b62218
GravesLogan <124059005+GravesLogan@users.noreply.github.com>
2023-04-22 19:47:47 -0400

    Changed the text on the about page to match the project

--- client/src/components/AboutUs.js
+++ client/src/components/AboutUs.js
@@ -5,20 +5,22 @@ function AboutUs() {
 		<div className='about-container'>
 			<h1>Our History</h1>
 			<p>
-				Around the summer of 2010, Fog Creek Software starts doing regular Creek Weeks, internal
-				explorations for potential products. In January 2011, a prototype that hopes to solve some
-				high-level planning issues is pitched. It's called Trellis. Full time development begins
-				soon after.
+				Throughout the course of the class we have been learning how to work with group members to create
+				web applications. Around the beginning of April we we're assigned with the task of implementing a full stack application
+				inside of our new group environment.
 			</p>
 
 			<h1>Our Mission</h1>
 			<p>
-				To provide top of the market information and investment opportunities so as to inspire our
-				host countries to be socially awakened.
+				To create a MERN application within a three week time span. We needed to implement CRUD operations 
+				as well as creating a fresh application that could be used in our daily lives.
 			</p>
 
 			<h1>Our Vision</h1>
-			<p>Connecting the real estate world through logic and faith.</p>
+			<p>
+				Create an application that would function as a task management tool. This application would be able to organize
+				the users workspaces as well as create and update task items. 
+			</p>
 		</div>
 	);
 }
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import Dashboard from './Dashboard';
 
 function HomePage() {
 	return (
 		<div className='App'>
-			<Dashboard/>
+			
 		</div>
 	);
 }

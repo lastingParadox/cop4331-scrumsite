efd2c6899b860153efb87d443ec6fd8216602af7
Anthony Cobb <anthonycobb@knights.ucf.edu>
2023-04-24 22:18:36 +0000

    updated service page with relevant info

--- client/src/components/ServiceView.js
+++ client/src/components/ServiceView.js
@@ -2,14 +2,14 @@ import "./View.css";
 
 import ViewData from "./ViewData";
 
-import ServiceView1 from "../assets/service-view1.jpg";
-import ServiceView2 from "../assets/service-view2.jpg";
+import ServiceView1 from "../assets/service-view-workspace.jpg";
+import ServiceView2 from "../assets/service-view-task.jpg";
 import ServiceView3 from "../assets/service-view3.jpg";
 
 function ServiceView() {
     return (
         <div className="view">
-            <h1>See work in a whole new way</h1>
+            <h1>Better than nothing</h1>
             <p>
                 View your team's projects from every angle and bring a fresh perspective to the task
                 at hand.
@@ -17,18 +17,18 @@ function ServiceView() {
             <div className="view-card-container">
                 <ViewData
                     image={ServiceView1}
-                    heading="Integrations"
-                    text="Connect the apps your team already uses into your Trello workflow or add a Power-Up to fine-tune your specific needs."
+                    heading="Workspaces"
+                    text="Seperate large projects into different workspaces to organize your workflow."
                 />
                 <ViewData
                     image={ServiceView2}
-                    heading="Butler Automation"
-                    text="No-code automation is built into every Trello board. Focus on the work that matters most and let the robots do the rest."
+                    heading="Tasks"
+                    text="Set goals for a project by creating tasks. Creating tasks lets you keep track of what should get done and what has been finished."
                 />
                 <ViewData
                     image={ServiceView3}
-                    heading="Trello Enterprise"
-                    text="The productivity tool teams love, paired with the features and security needed for scale."
+                    heading="Invite people to workspaces"
+                    text="Invite people to a workspace to coordinate project tasks with your team."
                 />
             </div>
         </div>

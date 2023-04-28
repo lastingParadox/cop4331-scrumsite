4cb2db45252fadb7137cc304a2fee4e0fed8afdf
Anthony Cobb <2blockdude@gmail.com>
2023-04-28 13:26:41 -0400

    Updated service page images and text

--- client/src/components/home/ServiceView.js
+++ client/src/components/home/ServiceView.js
@@ -4,7 +4,7 @@ import ViewData from "./ViewData";
 
 import ServiceView1 from "../../assets/service-view-workspace.jpg";
 import ServiceView2 from "../../assets/service-view-task.jpg";
-import ServiceView3 from "../../assets/service-view3.jpg";
+import ServiceView3 from "../../assets/service-view-invite.jpg";
 
 function ServiceView() {
     return (
@@ -18,12 +18,12 @@ function ServiceView() {
                 <ViewData
                     image={ServiceView1}
                     heading="Workspaces"
-                    text="See your projects from every angle with Board, Timeline, Table, Calendar, Dashboard, Map and Workspace views that will bring a fresh perspective to the task at hand."
+                    text="Create workspaces to seperate and organize your projects that will bring a fresh perspective to the task at hand."
                 />
                 <ViewData
                     image={ServiceView2}
                     heading="Tasks"
-                    text="Set goals for a project by creating tasks. Creating tasks lets you keep track of what should get done and what has been finished."
+                    text="Set goals for a project by creating tasks and organizing them into lists. Creating tasks lets you keep track of what should get done and what has been finished."
                 />
                 <ViewData
                     image={ServiceView3}
